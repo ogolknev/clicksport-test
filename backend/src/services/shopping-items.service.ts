fec0b37793ce8231f4aba17d1564425c334fe9c9
ogolknev <ogolknevnv@ya.ru>
2025-07-15 19:53:59 +0300

    add ShoppingItemsServiceDrizzle implementation

--- backend/src/services/shopping-items.service.ts
+++ backend/src/services/shopping-items.service.ts
@@ -1,16 +1,42 @@
+import { eq } from "drizzle-orm";
+import db from "../config/db";
+import { shoppingItemTable } from "../config/db/schema";
 import { ShoppingItem } from "../types/shooping-item";
 
 export interface ShoppingItemsService {
-  add(shoppingItem: ShoppingItem): Promise<ShoppingItem>
-  delete(id: NonNullable<ShoppingItem['id']>): Promise<void>
+  add(shoppingItem: ShoppingItem): Promise<ShoppingItem>;
+  delete(id: NonNullable<ShoppingItem["id"]>): Promise<void>;
 }
 
 export class ShoppingItemsServiceDrizzle implements ShoppingItemsService {
-  add(shoppingItem: ShoppingItem): Promise<ShoppingItem> {
-    throw new Error("Method not implemented.");
-  }
-  delete(id: NonNullable<ShoppingItem["id"]>): Promise<void> {
-    throw new Error("Method not implemented.");
-  }
+  async add(shoppingItem: ShoppingItem): Promise<ShoppingItem> {
+    const inserted = await db.insert(shoppingItemTable).values(shoppingItem);
 
+    if (!inserted.lastInsertRowid) {
+      throw new Error("Insert shopping item failed");
+    }
+
+    if (inserted.lastInsertRowid > BigInt(Number.MAX_SAFE_INTEGER)) {
+      throw new Error("inserted.lastInsertRowid > MAX_SAFE_INTEGER");
+    }
+    const id = Number(inserted.lastInsertRowid);
+
+    const [item] = await db
+      .select()
+      .from(shoppingItemTable)
+      .where(eq(shoppingItemTable.id, id));
+
+    if (!item) throw new Error("Inserted item not found");
+
+    return item;
+  }
+  async delete(id: NonNullable<ShoppingItem["id"]>): Promise<void> {
+    const deleted = await db
+      .delete(shoppingItemTable)
+      .where(eq(shoppingItemTable.id, id));
+
+    if (!deleted.rowsAffected) {
+      throw new Error("Failed delete shopping item");
+    }
+  }
 }
