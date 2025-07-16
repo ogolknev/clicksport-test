e6972af6438167d9de91bd7b0b2b121887651fa7
ogolknev <ogolknevnv@ya.ru>
2025-07-16 17:36:29 +0300

    add shopping item form design

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('web');
+  shoppingItemForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    price: new FormControl<number | null>(null),
+    currency: new FormControl<'RUB' | 'USD' | 'EUR'>('USD')
+  })
 }
