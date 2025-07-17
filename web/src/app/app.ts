9a5895e9294d0513378c09c13ef738d41163d477
ogolknev <ogolknevnv@ya.ru>
2025-07-17 20:33:03 +0300

    add pagination and form validation

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -5,17 +5,14 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { ShoppingListService } from './features/shopping-list/shopping-list.service';
-import {
-  ShoppingItem,
-} from './features/shopping-list/shopping-item.model';
+import { ShoppingItem } from './features/shopping-list/shopping-item.model';
 import { CurrencySymbolPipe } from './shared/pipes/currency-symbol.pipe';
 import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ReactiveFormsModule, CurrencySymbolPipe, DecimalPipe],
+  imports: [ReactiveFormsModule, CurrencySymbolPipe, DecimalPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -23,23 +20,75 @@ export class App {
   shoppingItemForm = new FormGroup({
     name: new FormControl('', Validators.required),
     description: new FormControl(''),
-    price: new FormControl('0', Validators.required),
+    price: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.pattern(/^\d+(?:[.,]\d{1,2})?$/),
+      ],
+    }),
     currency: new FormControl<'RUB' | 'USD' | 'EUR'>('USD'),
   });
 
   items = signal<ShoppingItem[]>([]);
+  errors = signal<string[]>([]);
+  status = signal<string>('');
+  pageSize = 5;
+  curPage = signal(1);
+  totalPages = signal(1);
 
   private shoppingListService = inject(ShoppingListService);
 
-  async onDelete(id: number) {
-    await this.shoppingListService.deleteItem(id);
-    this.shoppingListService.getItems().subscribe((response) => {
-      this.items.set(response.data);
+  async nextPage() {
+    this.curPage.update((p) => Math.min(p + 1, this.totalPages()));
+    await this.updateItems();
+  }
+
+  async prevPage() {
+    this.curPage.update((p) => Math.max(p - 1, 1));
+    await this.updateItems();
+  }
+
+  async updateItems() {
+    const response = await this.shoppingListService.getItems({
+      page: this.curPage(),
+      pageSize: this.pageSize,
     });
+    this.totalPages.set(
+      Math.ceil((response.meta?.pagination?.total ?? 1) / this.pageSize)
+    );
+    this.curPage.update((p) => Math.min(p, this.totalPages()));
+    this.items.set(response.data);
+  }
+
+  async onDelete(id: number) {
+    try {
+      await this.shoppingListService.deleteItem(id);
+      await this.updateItems();
+
+      this.status.set('Item successfully deleted');
+    } catch (err) {
+      throw console.error(err);
+    }
   }
 
   async onSubmit() {
-    if (this.shoppingItemForm.invalid) return;
+    if (this.shoppingItemForm.invalid) {
+      const errors: string[] = [];
+
+      if (this.shoppingItemForm.get('name')?.hasError('required')) {
+        errors.push('Name required');
+      }
+      if (this.shoppingItemForm.get('price')?.hasError('required')) {
+        errors.push('Price required');
+      }
+      if (this.shoppingItemForm.get('price')?.hasError('pattern')) {
+        errors.push('Invalid price');
+      }
+
+      this.errors.set(errors);
+
+      return;
+    }
 
     const value = this.shoppingItemForm.getRawValue() as {
       name: string;
@@ -48,21 +97,26 @@ export class App {
       currency: 'RUB' | 'USD' | 'EUR';
     };
 
-    await this.shoppingListService.createItem({
-      ...value,
-      price: parseFloat(value.price.replaceAll(',', '.')) * 100,
-    });
+    try {
+      await this.shoppingListService.createItem({
+        ...value,
+        price: parseFloat(value.price.replaceAll(',', '.')) * 100,
+      });
 
-    this.shoppingListService.getItems().subscribe((response) => {
-      this.items.set(response.data);
-    });
+      await this.updateItems();
+
+      this.status.set('Item successfully added');
+      this.errors.set([]);
+    } catch (err) {
+      if (err instanceof Error) this.errors.set([err.message]);
+    }
+
+    this.shoppingItemForm.reset({ currency: value.currency });
   }
 
   constructor() {
     effect(async () => {
-      this.shoppingListService.getItems().subscribe((response) => {
-        this.items.set(response.data);
-      });
+      await this.updateItems();
     });
   }
 }
--- web/src/app/features/shopping-list/shopping-list.service.ts
+++ web/src/app/features/shopping-list/shopping-list.service.ts
@@ -4,16 +4,18 @@ import { environment } from '../../../environments/environment';
 import { ShoppingItem, ShoppingItemCreate } from './shopping-item.model';
 import { APIResponse } from '../../../../../backend/src/types/api-response';
 import { APIPaginationQuery } from '../../types/api-pagination';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ShoppingListService {
   private http = inject(HttpClient);
 
-  getItems(pagination?: APIPaginationQuery) {
-    return this.http.get<APIResponse<ShoppingItem[]>>(
-      environment.apiUrl + '/shopping-items',
-      { params: pagination }
+  async getItems(pagination?: APIPaginationQuery) {
+    return firstValueFrom(
+      this.http.get<APIResponse<ShoppingItem[]>>(
+        environment.apiUrl + '/shopping-items',
+        { params: pagination }
+      )
     );
   }
 
@@ -25,13 +27,21 @@ export class ShoppingListService {
 
   async createItem(payload: ShoppingItemCreate) {
     return firstValueFrom(
-      this.http.post<APIResponse<ShoppingItem[]>>(
-        environment.apiUrl + '/shopping-items',
-        payload,
-        {
-          headers: { 'Content-Type': 'application/json' },
-        }
-      )
+      this.http
+        .post<APIResponse<ShoppingItem[]>>(
+          environment.apiUrl + '/shopping-items',
+          payload,
+          {
+            headers: { 'Content-Type': 'application/json' },
+          }
+        )
+        .pipe(
+          catchError((error) => {
+            return throwError(
+              () => new Error(`Failed to create item: ${error.message}`)
+            );
+          })
+        )
     );
   }
 }
